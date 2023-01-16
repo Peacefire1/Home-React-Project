2aa1c126f89b28ff60038c8dd3d8f14397f13ffd
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-01-16 18:01:35 +0800

    gert nav bar

---
+++ src/components/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+      <section className='head'>
+        <div className="container d_flex" >
+            <div className="left row">
+                <i className='fa fa-phone'></i>
+                <label>+97688888888</label>
+                <i className='fa fa-envelope'></i>
+                <label>PeaceFire1@gmail.com</label>
+            </div>
+            <div className="right row RText">
+                <label>Theme FAQ's</label>
+                <label>Need Helps</label>
+                <span>😀</span>
+                <label>EN</label>
+                <span>😀</span>
+                <label>US</label>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Head
---
+++ src/components/header/Search.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+// import logo from ""
+import { Link } from 'react-router-dom'
+const Search = () => {
+  return (
+    <div>
+      <section className='search'>
+        <div className='container c_flex'>
+          <div className="logo width">
+            {/* <img src={logo} alt="" /> */}
+          </div>
+
+          <div className="search-box f_flex">
+            <i className='fa fa-search'></i>
+            <input type="text" placeholder='Search and hit enter ...' />
+            <span>All Category</span>
+          </div>
+
+          <div className="icon f_flex width">
+            <i className='fa fa-user icon_circle'></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className='fa fa-shopping-bag icon-circle'></i>
+                <span>0</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Search
